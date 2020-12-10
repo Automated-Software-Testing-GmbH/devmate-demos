f28dfdd39d12be04402e67492c96882d8ddb4594
DavidTheilAst <david.theil@automated-software-testing.com>
2020-12-10 09:29:30 +0100

    Changes Default Config

--- speedometer/SpeedometerTest/Speedometer.AverageSpeedTest.cs
+++ speedometer/SpeedometerTest/Speedometer.AverageSpeedTest.cs
@@ -4,7 +4,7 @@
 * Test model: a8d26c27-a654-4d6d-857e-3e53b2919d0b
 */
 
-namespace AvgSpeed.Tests
+namespace AvgSpeedTests
 {
 	using System;
 	using System.Collections.Generic;
@@ -13,12 +13,12 @@ namespace AvgSpeed.Tests
 
 	public class AverageSpeedTestCase
 	{
-		private static AvgSpeed.ISpeedSensor CreateSensorStub()
+		private static AvgSpeed.ISpeedSensor SensorStub()
 		{
 			return new Mock<AvgSpeed.ISpeedSensor>().Object;
 		}
 
-		private static AvgSpeed.Preferences CreatePreferences(UnitsNet.Units.SpeedUnit preferredSpdUnit)
+		private static AvgSpeed.Preferences Preferences(UnitsNet.Units.SpeedUnit preferredSpdUnit)
 		{
 			return new AvgSpeed.Preferences
 			{
@@ -27,7 +27,7 @@ namespace AvgSpeed.Tests
 			};
 		}
 
-		private static AvgSpeed.ITrack CreateTrackStub(System.Int32 value, UnitsNet.Units.LengthUnit unit)
+		private static AvgSpeed.ITrack TrackStub(Int32 value, UnitsNet.Units.LengthUnit unit)
 		{
 			var trackMock = new Mock<AvgSpeed.ITrack>();
 			trackMock.Setup(track => track.Distance).Returns(new UnitsNet.Length(value, unit));
@@ -36,26 +36,26 @@ namespace AvgSpeed.Tests
 
 		private static IEnumerable<TestCaseData> PositiveTests()
 		{
-			yield return new TestCaseData(new AvgSpeed.Speedometer(CreateSensorStub(), CreatePreferences(UnitsNet.Units.SpeedUnit.KilometerPerHour)), new UnitsNet.Duration(2, UnitsNet.Units.DurationUnit.Hour), CreateTrackStub(60, UnitsNet.Units.LengthUnit.Kilometer), new UnitsNet.Speed(30, UnitsNet.Units.SpeedUnit.KilometerPerHour))
+			yield return new TestCaseData(new AvgSpeed.Speedometer(SensorStub(), Preferences(UnitsNet.Units.SpeedUnit.KilometerPerHour)), new UnitsNet.Duration(2, UnitsNet.Units.DurationUnit.Hour), TrackStub(60, UnitsNet.Units.LengthUnit.Kilometer), new UnitsNet.Speed(30, UnitsNet.Units.SpeedUnit.KilometerPerHour))
 				.SetName("p1")
 				.SetDescription("");
 		}
 
 		private static IEnumerable<TestCaseData> TestsThrowingException()
 		{
-			yield return new TestCaseData(new AvgSpeed.Speedometer(CreateSensorStub(), CreatePreferences(UnitsNet.Units.SpeedUnit.KilometerPerHour)), new UnitsNet.Duration(), CreateTrackStub(60, UnitsNet.Units.LengthUnit.Kilometer), typeof(ArgumentException))
+			yield return new TestCaseData(new AvgSpeed.Speedometer(SensorStub(), Preferences(UnitsNet.Units.SpeedUnit.KilometerPerHour)), new UnitsNet.Duration(), TrackStub(60, UnitsNet.Units.LengthUnit.Kilometer), typeof(ArgumentException))
 				.SetName("n1")
 				.SetDescription("duration: zero");
-			yield return new TestCaseData(new AvgSpeed.Speedometer(CreateSensorStub(), CreatePreferences(UnitsNet.Units.SpeedUnit.KilometerPerHour)), new UnitsNet.Duration(-1, UnitsNet.Units.DurationUnit.Minute), CreateTrackStub(60, UnitsNet.Units.LengthUnit.Kilometer), typeof(ArgumentException))
+			yield return new TestCaseData(new AvgSpeed.Speedometer(SensorStub(), Preferences(UnitsNet.Units.SpeedUnit.KilometerPerHour)), new UnitsNet.Duration(-1, UnitsNet.Units.DurationUnit.Minute), TrackStub(60, UnitsNet.Units.LengthUnit.Kilometer), typeof(ArgumentException))
 				.SetName("n2")
 				.SetDescription("duration: negative");
-			yield return new TestCaseData(new AvgSpeed.Speedometer(CreateSensorStub(), CreatePreferences(UnitsNet.Units.SpeedUnit.KilometerPerHour)), new UnitsNet.Duration(2, UnitsNet.Units.DurationUnit.Hour), CreateTrackStub(0, UnitsNet.Units.LengthUnit.Centimeter), typeof(ArgumentException))
+			yield return new TestCaseData(new AvgSpeed.Speedometer(SensorStub(), Preferences(UnitsNet.Units.SpeedUnit.KilometerPerHour)), new UnitsNet.Duration(2, UnitsNet.Units.DurationUnit.Hour), TrackStub(0, UnitsNet.Units.LengthUnit.Centimeter), typeof(ArgumentException))
 				.SetName("n3")
 				.SetDescription("track: zero");
-			yield return new TestCaseData(new AvgSpeed.Speedometer(CreateSensorStub(), CreatePreferences(UnitsNet.Units.SpeedUnit.KilometerPerHour)), new UnitsNet.Duration(2, UnitsNet.Units.DurationUnit.Hour), CreateTrackStub(-10, UnitsNet.Units.LengthUnit.Foot), typeof(ArgumentException))
+			yield return new TestCaseData(new AvgSpeed.Speedometer(SensorStub(), Preferences(UnitsNet.Units.SpeedUnit.KilometerPerHour)), new UnitsNet.Duration(2, UnitsNet.Units.DurationUnit.Hour), TrackStub(-10, UnitsNet.Units.LengthUnit.Foot), typeof(ArgumentException))
 				.SetName("n4")
 				.SetDescription("track: negative");
-			yield return new TestCaseData(new AvgSpeed.Speedometer(CreateSensorStub(), CreatePreferences(UnitsNet.Units.SpeedUnit.KilometerPerHour)), new UnitsNet.Duration(2, UnitsNet.Units.DurationUnit.Hour), null, typeof(ArgumentNullException))
+			yield return new TestCaseData(new AvgSpeed.Speedometer(SensorStub(), Preferences(UnitsNet.Units.SpeedUnit.KilometerPerHour)), new UnitsNet.Duration(2, UnitsNet.Units.DurationUnit.Hour), null, typeof(ArgumentNullException))
 				.SetName("n5")
 				.SetDescription("track: nullPointer");
 		}
@@ -81,17 +81,17 @@ namespace AvgSpeed.Tests
 WARNING: Modification of this comment will make it impossible to merge user-defined changes
 
 BEGIN_CODEGEN_DATA
-H4sIAAAAAAAAAN2WW2/aMBSAn0HiP1g8hQpCgUJvayXEZUUbFynZwzTtwSSn1GpiZ7ZDy6b999luoClpKWVTt07i4tg
-+t++c2Ke6V8jvoe4YjcYuGo67g/5n5F4MHNQZD4e9katX3wMFjiX4aLpAPsxDNdbzLgiJQuZDcILwkV9vefXDCm41Dy
-oHfsuvHDUPodKAZmNaP64d+/tTJVMt5At5ikMQEfYAteczJwLwba1KFPI/CvlcLAidIWchJISna892hwUBeJIwKmzjF
-vHu94w+USLtPlfqbxi/PtW2clE8DYiHvAALoeypQGZgbGqTHSxUJDltNhdxMleBISGxVAIr1wbmzwEqGEcdDmrP3YMj
-46lV0qJGPletInesSJIwCiAEKtEl9iTjC7Mqrzi7QRRulqGMmBwsd4Lfu/Ug0nFZJR1Q7qdx/kmnJhwugQP1QCQ+pWY
-szUGMQNpmYBsRPUSR2cRVOJGZ+EveD1yOvevEcTM2LBPVAyobdTTHQQxltBbKR6AzeWViiV/d/0GPxqEqoGkA75bV08
-USn6MJE0SSOZgyTtfEgkDgIw4y5tSYT8tZemIFxfyyECRwK1tm5Rek2f5AgjtFE+AXLOYlJa5trQS6sXqfdbj1DOHlk
-lFkhMuZRLX2NyTm3vq6VeOg1cgKb/LcYMzZDsiReq+tYlQrpua6IDxO7lJXLO6WPJM0VxeIOkRSlfB2svhbKZKLCNil
-1eazWL8MKwBZ9PRp9H7iygn6DpwlifjnuVVqm8t/SGgs4bXo1regS2GG9UHzVgjvdr5spNtRGMkOeBtP4pXa8Nuq3N2
-4VmobyfYZky9jevAc0/+zXmkcBBlOIzW5iVXzWVZKwYQR1Y/whzfZFw3p62qkcTnKEU9pfdB4FEtmU9LxzhnxMw2v9R
-heRKi6JBXCcpYIWp495Uz7Ztwur13vCG4j1aCDn7o+13aotizGATpbmbXTXlr3Bo2BpHDaQgCXdptD75uStpZmyom6b
-YE9ftlvQS7bIPx5lK6qqMcAJsEbF0QqdKuEzs63xLgCpL/q8wuEirFt+Q0AAA==
+H4sIAAAAAAAAAN2WW2/aMBTHn0HiOxzxFCoIBQq9rZUQ0DXauEhkD9O0h5CcUquJndkOLZv23We7QFPSUlrtgiaRxDg
++Pv/zOye2q3uF/B50hzAYutAfdp2Lz+BeOmPoDPv93sDVb98jRe5JDGAyhwBnkWrrfheFhIgFGJ6AdxTUW379sOK1mg
+eVg6AVVI6ah1hpYLMxqR/XjoP9ibKpFvKFPPUiFLHnI7Rn03GMGOiZRCH/o5DPJYLQKYznQmJ0uvbf7rAwRF8SRoVtV
+BH/YczgEyXSvuBq9lvGb061q1ycTELigx96Qih3Ko4prlx2PKECyWm3uZiTmYoLhPSkMlgqsx3zGCMVjMP9YyyTiVXS
+RsYyV62CO1QISRSHGCGVcOX5kvG5eSuvObsFirfLIAZMOsuRGPTufIx1RFZJh5L7aWQ/K2fE8Qo5Uh8FpNqWjl0MUNq
+mYZvBugmxGcRVCLHp+Ee6HZd7/g2Yu+HnUNmow8wLEyzDmvqPSKfy2shP/rpkp0eTSNXJJMR3yyLpetI7hxETRJIZmm
+pNF8CcYBgAR5lwatyn7SzdseJg7ixCidxKV1N5q2zaH0h4bzxCfskSXlKGev6VQTdRX6oOsZ6hunxlJjLG5VQ+Wvsb0
+vDgd92fkWY1ssabNBtoOXuMcqA+VqsY14qpvi4Kn5P7RBWLb0uVSZGry0GtDKm873bO3pgQOY+RXVltPk10oa/CzYKm
+z4MOFiJO4DtytsC+k5Qqtc2l3Sc0kfjnWda3YElx6uklY5d5vnal2Miyo6CRN8BsPAtTase7X5WvpVipbeR4wZh8HcG
+Dlwj+P7VIkzDMsBmozk18mi/yUROMGFEnBf541/miwXxdtTSisRLiq1kfHQmKJTNoceScMRJkTpzWU0iBULWhKYTlLB
+FYriXlzFnKyC6vbcWAd7E6IWOQ2urWRqgDU+KFcLZya6dVWg8OjYNFsbSFQC7tNsfeN2VtLd2UF9NtC+zpjXkLctnN/
+PejdFVFPQVwEbyRIFKhWyU4O98S4wqQvtTvF2TtQcB5DQAA
 END_CODEGEN_DATA
 */
