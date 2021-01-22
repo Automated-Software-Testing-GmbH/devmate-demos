0c4751dcef599e95b185a446d16fb370f222777a
DavidTheilAst <david.theil@automated-software-testing.com>
2021-01-22 14:04:41 +0100

    = added

--- meeting_calculator/MeetingCalculator/AppointmentChecker.cs
+++ meeting_calculator/MeetingCalculator/AppointmentChecker.cs
@@ -25,11 +25,11 @@ namespace MeetingCalculator
                 if (cur.StartTime.Ticks >= cur.EndTime.Ticks) throw new Exception("Start must be before End");
                 if (meeting.StartTime.Ticks >= meeting.EndTime.Ticks) throw new Exception("Start Time of Meeting2Check must be before End Time");
 
-                if (cur.StartTime.Ticks < meeting.StartTime.Ticks && meeting.StartTime.Ticks < cur.EndTime.Ticks)
+                if (cur.StartTime.Ticks <= meeting.StartTime.Ticks && meeting.StartTime.Ticks < cur.EndTime.Ticks)
                 {
                     return false;
                 }
-                if (cur.StartTime.Ticks < meeting.EndTime.Ticks && meeting.EndTime.Ticks < cur.EndTime.Ticks)
+                if (cur.StartTime.Ticks < meeting.EndTime.Ticks && meeting.EndTime.Ticks <= cur.EndTime.Ticks)
                 {
                     return false;
                 }
